e94eeb913ce0085decbe937cee923d65639f87ea
DelkDang <93030567+DelkDang@users.noreply.github.com>
2022-02-19 20:03:02 +0700

    UI

--- demo/src/App.java
+++ demo/src/App.java
@@ -3,6 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import org.json.JSONObject;
 
 
@@ -10,16 +14,43 @@ public class App {
     public static void main(String[] args) throws IOException
     {
         String[] arr = new String[] {"bitcoin", "ethereum", "tether", "binancecoin", "usd-coin", "ripple", "cardano", "solana", "avalanche-2", "terra-luna"};
-        for(int i = 0; i < arr.length; i++)
-        {
-            String coin = arr[i];
-            double price = getPriceCoin(coin);
-            System.out.println("price of " + coin + " = " + price);
-        }
-        
-    }
+        int len = arr.length;
 
-    private static double getPriceCoin(String coin) throws IOException
+        JFrame window = new JFrame();
+        JPanel mainPanel = new JPanel();
+        JPanel coinPanel = new JPanel();
+        JPanel pricePanel = new JPanel();
+        window.setTitle("Demo code");
+        window.setSize(800, 600);
+        window.setLocationRelativeTo(null);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // set layout
+        BorderLayout winLayout = new BorderLayout();
+        GridLayout gLayout1 = new GridLayout(1, 2);
+        GridLayout gLayout2 = new GridLayout(len, 1);
+        window.setLayout(winLayout);
+        mainPanel.setLayout(gLayout1);
+        coinPanel.setLayout(gLayout2);
+        pricePanel.setLayout(gLayout2);
+
+        window.add(mainPanel, BorderLayout.CENTER);
+        mainPanel.add(coinPanel);
+        mainPanel.add(pricePanel);
+        
+
+        JLabel[] priceLabel = new JLabel[len];
+        for(int i = 0; i < len; i++)
+        {
+            JLabel coinLabel = new JLabel(arr[i]);
+            priceLabel[i] = new JLabel(getPriceOf(arr[i]));
+            pricePanel.add(priceLabel[i]);
+            coinPanel.add(coinLabel);
+        }
+        window.setVisible(true);
+    }
+    
+
+    private static String getPriceOf(String coin) throws IOException
     {
         String getURL = "https://api.coingecko.com/api/v3/simple/price?ids=" + coin + "&vs_currencies=usd";
         double price = -1;
@@ -33,14 +64,12 @@ public class App {
             String input;
             StringBuffer data = new StringBuffer();
             while((input = in.readLine()) != null)
-            {
                 data.append(input);
-            }
             in.close();
             JSONObject obj = new JSONObject(data.toString());
             price = obj.getJSONObject(coin).getDouble("usd");
         }
-        return price;
+        return Double.toString(price);
     }
 }
  
